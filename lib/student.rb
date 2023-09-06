6485c26011e68bd234aeaebe75ad1ee9a07111ab
Taylor Pubins <tpubz@icloud.com>
2023-09-06 08:31:07 -0700

    Iteration 1

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,19 @@
 class Student
-  
+  attr_reader :name,
+              :age,
+              :scores
+
+  def initialize(args)
+    @name = args[:name]
+    @age = args[:age]
+    @scores = []
+  end
+
+  def log_score(score)
+    @scores << score
+  end
+
+  def grade
+    @scores.reduce { |tot, score| tot + score }.to_f / @scores.size
+  end
 end
