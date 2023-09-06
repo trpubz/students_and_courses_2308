e10bc5ca71bd0c360d75fd078da711e806e72847
Taylor Pubins <tpubz@icloud.com>
2023-09-06 08:41:55 -0700

    Iteration 2

--- lib/course.rb
+++ lib/course.rb
@@ -1,3 +1,19 @@
 class Course
-  
+  attr_reader :name,
+              :capacity,
+              :students
+
+  def initialize(name, capacity)
+    @name = name
+    @capacity = capacity
+    @students = []
+  end
+
+  def full?
+    @students.size >= @capacity
+  end
+
+  def enroll(student)
+    @students << student
+  end
 end
--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -3,5 +3,32 @@ require './lib/course'
 require './lib/student'
 
 RSpec.describe Course do
-  
+  describe '#initialize' do
+    it 'exists' do
+      course = Course.new("Calculus", 2)
+      expect(course).to be_a Course
+    end
+  end
+
+  describe '@attributes' do
+    it 'has attributes' do
+      course = Course.new("Calculus", 2)
+      expect(course.name).to eq "Calculus"
+      expect(course.capacity).to eq 2
+      expect(course.students).to eq []
+      expect(course.full?).to eq false
+    end
+  end
+
+  describe '#enroll' do
+    it 'enrolls students' do
+      course = Course.new("Calculus", 2)
+      student1 = Student.new({name: "Morgan", age: 21})
+      student2 = Student.new({name: "Jordan", age: 29})
+      course.enroll(student1)
+      course.enroll(student2)
+      expect(course.students.size).to eq 2
+      expect(course.full?).to eq true
+    end
+  end
 end
